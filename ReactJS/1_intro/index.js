6b4582ea507c379d8f46fc82f3a3745d6c57694f
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-09-12 19:09:56 +0530

    First 2 lectures, basic intro

---
+++ ReactJS/1_intro/index.js
@@ -0,0 +1,29 @@
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+  },
+  "This is heading One"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "heading2",
+  },
+  "This is heading Two"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
+
+// console.log(container);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
